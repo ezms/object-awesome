ff532b9bf6aaa21e250b21d6e96f8f58f02011d4
ezms <emanuelm.dev@gmail.com>
2023-12-13 19:16:12 -0300

    feat: create Objects class with utilities methods :hammer:

---
+++ src/modules/Objects/index.ts
@@ -0,0 +1,27 @@
+export class Objects {
+	public static nonNull(anything: any): boolean {
+		return anything !== null && typeof anything !== 'undefined';
+	}
+
+	public static isNull(anything: any): boolean {
+		return anything === null || typeof anything === 'undefined';
+	}
+
+	public static requireNonNull(...stuff: any): boolean {
+		if (this.nonNull(stuff)) return true;
+		throw new TypeError(`The object(s) ${stuff} has null value(s)`);
+	}
+
+	public static equals(item1: any, item2: any): boolean {
+		return item1 === item2 || this.nonNull(item1) === item2;
+	}
+
+	public static strictEquals(item1: any, item2: any) {
+		return this.nonNull(item1) === this.nonNull(item2);
+	}
+
+	public static clone(obj: any): void {
+		if (obj instanceof Object) return { ...obj };
+		throw new Error('Object not cloneable');
+	}
+}
