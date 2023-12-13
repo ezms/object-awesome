b877547d78fe3a4dc7b350c4cd25b95329b0486d
ezms <emanuelm.dev@gmail.com>
2023-12-13 19:51:32 -0300

    feat: upload last Objects class methods tests :white_check_mark:

--- src/resources/tests/modules/Objects/index.spec.ts
+++ src/resources/tests/modules/Objects/index.spec.ts
@@ -22,6 +22,44 @@ describe('Objects class tests', () => {
 		expect(Objects.isNull(42)).toBe(false);
 	});
 
+	it('requireNonNull method should return true for non-null stuff', () => {
+		expect(Objects.requireNonNull(`I'm not null >:(`)).toBe(true);
+		expect(Objects.requireNonNull(`I'm not undefined >:(`)).toBe(true);
+	});
+
+	it('requireNonNull method should throw an error when pass null or undefined values', () => {
+		expect(Objects.requireNonNull(null)).toThrow(TypeError);
+		expect(Objects.requireNonNull(undefined)).toThrow(TypeError);
+	});
+
+	it('equals should return true for equal values or non-null and equal values', () => {
+		const value1 = 'some value';
+		const value2 = 'some value';
+
+		expect(Objects.equals(value1, value2)).toBe(true);
+	});
+
+	it('equals should return false for non-equal values and null or undefined values', () => {
+		const value1 = 'some value';
+		const value2 = 'another value';
+
+		expect(Objects.equals(value1, value2)).toBe(false);
+		expect(Objects.equals(value1, null)).toBe(false);
+		expect(Objects.equals(value1, undefined)).toBe(false);
+	});
+
+	it('strictEquals should return true for strictly equal non-null values', () => {
+		const value1 = 'some value';
+		const value2 = new String('some value');
+		expect(Objects.strictEquals(value1, value2)).toBe(true);
+	});
+
+	it('strictEquals should return false for non-strictly equal values or null or undefined values', () => {
+		const value1 = 'some value';
+		expect(Objects.strictEquals(value1, null)).toBe(false);
+		expect(Objects.strictEquals(value1, undefined)).toBe(false);
+	});
+
 	it('clone method should throw an error for non-cloneable objects', () => {
 		expect(() => Objects.clone(Object.create(null))).toThrow(
 			'Object not cloneable',
